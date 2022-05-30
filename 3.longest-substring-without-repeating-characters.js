ee4c4b273ab8f35a15da3a7d8661d2e6b3b87a88
sanjeevyg <yogi.sjv@gmail.com>
2022-05-30 16:03:11 -0400

    longest substring algo prob optimized

--- 3.longest-substring-without-repeating-characters.js
+++ 3.longest-substring-without-repeating-characters.js
@@ -10,35 +10,28 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let set = new Set();
-    let maxLength = 0;
-    let substrLength = 0;
+    
+   let history = {};
+   let start = 0;
+   let max = 0;
 
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === s[i - 1]) {
-            substrLength = 0;
-            set.clear()
-            // set.add(s[i])
-        }
-
-        if(set.has(s[i])) {
-            substrLength = 0;
-            set.clear()
-            continue;
-        }
-        set.add(s[i])
-        substrLength = substrLength + 1
-        // console.log("subs", substrLength)
-        maxLength = Math.max(substrLength, maxLength)
-    }
-    return maxLength
+   for(let end = 0; end < s.length; end++) {
+       let char = s[end];
+       let index = history[char];
+       if(index !== undefined && index >= start) {
+           start = index + 1;
+       }
+       max = Math.max(max, end - start + 1);
+       history[char] = end;
+   }
+   return max;
 };
-// console.log(lengthOfLongestSubstring("abcabcbb"))
-// console.log(lengthOfLongestSubstring(""))
-// console.log(lengthOfLongestSubstring(" "))
-// console.log(lengthOfLongestSubstring("pwwkew"))
-// console.log(lengthOfLongestSubstring("aab"))
-// console.log(lengthOfLongestSubstring("bbbbb"))
-console.log(lengthOfLongestSubstring("dvdf"))
+// console.log(lengthOfLongestSubstring("abcabcbb")) //3
+// console.log(lengthOfLongestSubstring("")) //0
+// console.log(lengthOfLongestSubstring(" ")) //1
+// console.log(lengthOfLongestSubstring("pwwkew")) //3
+// console.log(lengthOfLongestSubstring("aab")) //2
+// console.log(lengthOfLongestSubstring("bbbbb")) //1
+// console.log(lengthOfLongestSubstring("dvdf")) //3
 // @lc code=end
 
