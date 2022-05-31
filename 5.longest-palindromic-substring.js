a9a8767ee096e95ef3ee75626ce0d5a4ce5fa2cd
sanjeevyg <yogi.sjv@gmail.com>
2022-05-30 20:59:45 -0400

    longestPalindrome

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -10,7 +10,26 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
-};
+    let max = "";
+    for(let i = 0; i < s.length; i++) {
+        for(let j of [0, 1]) {
+            let left = i;
+            let right = i + j; 
+        
+            while(s[left] && s[left] === s[right]) {
+                left--;
+                right++;
+            }
+            if((right - left - 1)  > max.length) {
+            max = s.substring(left + 1, right);
+        }
+        }
+    }
+    return max;
+}
+
+// console.log(longestPalindrome("cbbd"))
+// console.log(longestPalindrome("babad"))
+// console.log(longestPalindrome("abcda"))
 // @lc code=end
 
