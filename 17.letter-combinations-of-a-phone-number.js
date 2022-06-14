d15fa229023a5614cb6653502e15b289a354539f
sanjeevyg <yogi.sjv@gmail.com>
2022-06-13 22:45:51 -0400

    generateParenthesis

--- 17.letter-combinations-of-a-phone-number.js
+++ 17.letter-combinations-of-a-phone-number.js
@@ -10,37 +10,38 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    if (digits == null || digits.length === 0) return [];
+   if(digits.length === 0 || digits === null) return [];
 
-  const map = {
-    2: 'abc',
-    3: 'def',
-    4: 'ghi',
-    5: 'jkl',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz',
-  };
-  
-  const res = [];
-  const go = (i, s) => {
-    // console.log("[i, s]", [i, s])
-    if (i === digits.length) {
-        res.push(s);
-        return;
-    }
+   let map = {
+     1: "",
+     2: "abc",
+     3: "def",
+     4: "ghi",
+     5: "jkl",
+     6: "mno",
+     7: "pqrs",
+     8: "tuv",
+     9: "wxyz"
+   }
 
-    for (const c of map[digits[i]]) {
-        // console.log(s)
-        go(i + 1, s + c);
-    }
-  };
+   let res = [];
 
-  go(0, '');
-  return res;
-    
+   let go = (i, s) => {
+     if(i === digits.length) {
+       res.push(s);
+       return;
+     }
+
+     for(let c of map[digits[i]]) {
+       go(i + 1, s + c)
+     }
+   }
+
+   go(0, "")
+
+   return res
 };
 // console.log(letterCombinations("23"))
+//[ad, 
 // @lc code=end
 
