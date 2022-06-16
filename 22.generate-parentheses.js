87c4bdff807c39e584b8848d7f05ca0667b1a5af
sanjeevyg <yogi.sjv@gmail.com>
2022-06-15 22:29:32 -0400

    generate-parentheses

--- 22.generate-parentheses.js
+++ 22.generate-parentheses.js
@@ -10,7 +10,31 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
+    const res = [];
     
+    const go = (l, r, s ) => { // l: left remaining, r: right remaining
+    if (l > r) return; // Validate by the number of '(' should be always >= ')'
+
+
+    if (l === 0 && r === 0) {
+        res.push(s);
+        // console.log("f", s, count)
+      return;
+    }
+    // count++
+
+    if (l > 0) go(l - 1, r, s + '(');
+    // console.log("l", s, count)
+    if (r > 0) go(l, r - 1, s + ')');
+    // console.log("r", s, count)
+  };
+
+  go(n, n, '');
+  return res;
+
 };
+
+
+// console.log(generateParenthesis(3))
 // @lc code=end
 
---
+++ 494.target-sum.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=494 lang=javascript
+ *
+ * [494] Target Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function(nums, target) {
+    
+};
+// @lc code=end
+
